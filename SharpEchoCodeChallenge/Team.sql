ae0e5818bf4e789ffdd7ca8ec44caf30940db61d
Harsha kumar G H <harshakumargh@gmail.com>
2022-11-30 12:17:48 -0600

    Update Team.sql

--- SharpEchoCodeChallenge/Team.sql
+++ SharpEchoCodeChallenge/Team.sql
@@ -22,16 +22,3 @@ CREATE TABLE [dbo].[Matches]
     [ModifiedBy] NVARCHAR(50) DEFAULT 'System',
     [ModifiedOn] Date DEFAULT GETDATE()
 )
-
-INSERT INTO[dbo].[Matches](Date,Team1,Team2,Winner) VALUES ('09/20/2022',1,2,1)
-
-DECLARE @Team1 INT = 1
-DECLARE @Team2 INT = 2
-
-select sum(team1wins) as Team1, sum(team2wins) as Team2
-from ((select team1, team2,
-               (case when winner = @Team1 then 1 else 0 end) as team1wins,
-			   (case when winner = @Team2 then 1 else 0 end) as team2wins
-        from Matches where (team1 =@Team1 and team2 = @Team2 ) OR(team1 =@Team2 and team2 = @Team1)
-       )
-      ) t
